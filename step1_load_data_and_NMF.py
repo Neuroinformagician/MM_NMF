e142b8941e56f72d83d549754a6c70ddd194896e
Neuroinformagician <124949541+Neuroinformagician@users.noreply.github.com>
2024-11-16 00:28:02 +0900

    add .py

---
+++ step1_load_data_and_NMF.py
@@ -0,0 +1,110 @@
+# step1. load data and NMF
+
+import pickle
+from scipy.cluster.hierarchy import dendrogram, leaves_list, linkage
+
+import matplotlib.pyplot as plt
+from matplotlib import font_manager
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.metrics import (auc, confusion_matrix, f1_score, matthews_corrcoef,
+                             precision_score, roc_curve)
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import NMF
+
+seed = None
+
+def load_data(filepath):
+    try:
+        df_ori = pd.read_csv(filepath, index_col=0)
+        return df_ori
+    except FileNotFoundError:
+        print(f"The file {filepath} was not found.")
+        return None
+    except pd.errors.ParserError as e:
+        print(f"Error parsing the file {filepath}: {e}")
+        return None
+
+def select_columns(df, column_names):
+    return df[column_names]
+
+df_names = [
+    "MGC_ptosis", "MGC_diplopia", "MGC_eyelid_closure", "MGC_speech", "MGC_chewing",
+    "MGC_swallowing", "MGC_respiration", "MGC_neck", "MGC_upper_limb", "MGC_lower_limb",
+    "MGADL_speech", "MGADL_chewing", "MGADL_swallowing", "MGADL_respiration",
+    "MGADL_toothbrushing", "MGADL_getting_up", "MGADL_diplopia", "MGADL_ptosis",
+    "MGQOL1_dissatisfaction", "MGQOL2_seeing", "MGQOL3_eating",
+    "MGQOL4_social_activity_restriction", "MGQOL5_hobby_entertainment", "MGQOL6_family_role",
+    "MGQOL7_behavior_modification", "MGQOL8_work_impact", "MGQOL9_speaking",
+    "MGQOL10_driving", "MGQOL11_feeling_down", "MGQOL12_walking", "MGQOL13_quick_action",
+    "MGQOL14_mental_crushing", "MGQOL15_dressing"
+]
+
+config = {
+    "filepath": "./data/df_4th.csv",
+}
+
+df_ori = load_data(config["filepath"])
+
+if df_ori is not None:
+    df = select_columns(df_ori, df_names)
+
+numeric_columns = df.select_dtypes(include='number')
+scaler = MinMaxScaler()
+scaled_columns = scaler.fit_transform(numeric_columns)
+
+scaled_df = df.copy()
+scaled_df[numeric_columns.columns] = scaled_columns
+
+nmf = NMF(n_components=4, init='nndsvd', max_iter=200, random_state=0)
+
+W = nmf.fit_transform(scaled_df)
+H = nmf.components_
+
+
+def cluster_data(matrix, method='complete', metric='euclidean'):
+    cluster_rows = linkage(matrix, method=method, metric=metric)
+    return leaves_list(cluster_rows)
+
+feature_names = scaled_df.columns
+
+row_order = cluster_data(H)
+col_order = cluster_data(H.T)
+
+
+H_reordered = H[row_order][:, col_order]
+feature_names_reordered = feature_names[col_order]
+
+module_names = ["module QOL", "module Diplopia", "module Ptosis", "module Systemic"]
+module_names_reordered = [module_names[i] for i in row_order]
+
+IDs = df_ori['ID']
+W_with_id = np.column_stack((IDs, W))
+module_names_with_id = ['ID'] + module_names_reordered
+W_with_id_df = pd.DataFrame(W_with_id, columns=module_names_with_id)
+
+module_names_with_id = ['ID', "module QOL", "module Diplopia", "module Ptosis", "module Systemic"]
+
+W_with_id_df = pd.DataFrame(W_with_id, columns=module_names_with_id)
+MM_data = df_ori[["ID", "MMorbetter"]]
+
+W_MM = pd.merge(W_with_id_df, MM_data, on="ID")
+W_MM.index = W_MM['ID']
+W_MM.drop(columns=['ID'], inplace=True)
+
+with open('./out/nmf_model.pkl', 'wb') as file:
+    pickle.dump(nmf, file)
+
+with open('./out/W_matrix.pkl', 'wb') as file:
+    pickle.dump(W, file)
+
+with open('./out/H_matrix.pkl', 'wb') as file:
+    pickle.dump(H, file)
+
+with open('./out/W_with_id_df.pkl', 'wb') as file:
+    pickle.dump(W_with_id_df, file)
+
+with open('./out/W_MM.pkl', 'wb') as file:
+    pickle.dump(W_MM, file)
---
+++ step2_prediction_with_NMF.py
@@ -0,0 +1,225 @@
+# step2. prediction with NMF matrix
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import pickle
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
+
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import roc_curve, auc
+
+seed = None
+
+with open("./out/nmf_model.pkl", "rb") as file:
+    nmf = pickle.load(file)
+
+with open("./out/W_matrix.pkl", "rb") as file:
+    W = pickle.load(file)
+
+with open("./out/H_matrix.pkl", "rb") as file:
+    H = pickle.load(file)
+
+with open("./out/W_with_id_df.pkl", "rb") as file:
+    W_with_id_df = pickle.load(file)
+
+with open("./out/W_MM.pkl", "rb") as file:
+    W_MM = pickle.load(file)
+
+
+X_train_list = []
+y_train_list = []
+X_test_list = []
+y_test_list = []
+
+
+kf = KFold(n_splits=5, random_state=seed, shuffle=True)
+
+for train_index, test_index in kf.split(W_MM):
+    W_train, W_test = W_MM.iloc[train_index], W_MM.iloc[test_index]
+    
+    X_train = W_train[["module QOL", "module Diplopia", "module Ptosis", "module Systemic"]]
+    y_train = W_train["MMorbetter"]
+
+    X_test = W_test[["module QOL", "module Diplopia", "module Ptosis", "module Systemic"]]
+    y_test = W_test["MMorbetter"]
+
+    X_train_list.append(X_train)
+    y_train_list.append(y_train)
+    X_test_list.append(X_test)
+    y_test_list.append(y_test)
+
+
+def save_to_pickle(data, filename):
+    with open(filename, "wb") as f:
+        pickle.dump(data, f)
+
+save_to_pickle(X_train_list, "./out/X_train_list.pkl")
+save_to_pickle(y_train_list, "./out/y_train_list.pkl")
+save_to_pickle(X_test_list, "./out/X_test_list.pkl")
+save_to_pickle(y_test_list, "./out/y_test_list.pkl")
+
+param_grids = {
+    "svm": {
+        "C": [0.1, 1, 10, 100],
+        "kernel": ["linear", "rbf"],
+        "gamma": ["auto", "scale"]
+    },
+    "logreg": [
+        {
+            "penalty": ["l1", "l2"],
+            "C": [0.1, 1, 10, 100],
+            "fit_intercept": [True, False],
+            "solver": ["liblinear", "saga"],
+        },
+        {
+            "penalty": ["elasticnet"],
+            "C": [0.1, 1, 10, 100],
+            "fit_intercept": [True, False],
+            "l1_ratio": [0, 0.25, 0.5, 0.75, 1],
+            "solver": ["saga"],
+        },
+        {
+            "penalty": [None],
+            "fit_intercept": [True, False],
+            "solver": ["newton-cg", "lbfgs", "sag", "saga"],
+        }
+    ],
+    "rf": {
+        "n_estimators": [10, 50, 100, 200],
+        "max_depth": [None, 10, 20, 30],
+        "min_samples_split": [2, 5, 10],
+        "min_samples_leaf": [1, 2, 4]
+    }
+}
+
+best_params_list = []  
+best_scores_list = []  
+predicted_probs_list = {
+    "SVM": [],
+    "Logistic Regression": [],
+    "Random Forest": [],
+    "Naive Bayes": []
+}
+
+models = {
+    "SVM": svm.SVC(probability=True, random_state=seed),
+    "Logistic Regression": LogisticRegression(random_state=seed),
+    "Random Forest": RandomForestClassifier(random_state=seed),
+    "Naive Bayes": GaussianNB()
+}
+
+for X_train, y_train, X_test, y_test in zip(X_train_list, y_train_list, X_test_list, y_test_list):
+    
+    tuned_models = {}
+    best_scores = {}
+    
+    for model_name, model in models.items():
+        if model_name != "Naive Bayes":
+            grid_search = GridSearchCV(
+                estimator=model,
+                param_grid=param_grids.get(model_name.lower(), {}),
+                cv=5,
+                scoring="accuracy",
+                n_jobs=-1
+            )
+            grid_search.fit(X_train, y_train)
+            tuned_models[model_name] = grid_search.best_estimator_
+            best_scores[model_name] = grid_search.best_score_
+        else:
+            model.fit(X_train, y_train)
+            tuned_models[model_name] = model
+            best_scores[model_name] = model.score(X_train, y_train) 
+    
+    best_params = {name: model.get_params() for name, model in tuned_models.items() if name != "Naive Bayes"}
+    
+    best_params_list.append(best_params)
+    best_scores_list.append(best_scores)
+    
+    for model_name, model in tuned_models.items():
+        predicted_probs = model.predict_proba(X_test)[:, 1]
+        predicted_probs_list[model_name].append(predicted_probs)
+
+def save_mean_roc_data(y_test_list, model_probs_list, model_name, roc_data_dict):
+    mean_fpr = np.linspace(0, 1, 100)
+    mean_tpr = 0.0
+    
+    for y_test, probas in zip(y_test_list, model_probs_list):
+        fpr, tpr, thresholds = roc_curve(y_test, probas)
+        mean_tpr += np.interp(mean_fpr, fpr, tpr)
+    
+    mean_tpr /= len(y_test_list)
+    mean_auc = auc(mean_fpr, mean_tpr)
+    
+    roc_data_dict[model_name] = {
+        "mean_fpr": mean_fpr,
+        "mean_tpr": mean_tpr,
+        "mean_auc": mean_auc
+    }
+
+
+roc_data = {}
+
+save_mean_roc_data(y_test_list, predicted_probs_list["SVM"], "SVM", roc_data)
+save_mean_roc_data(y_test_list, predicted_probs_list["Logistic Regression"], "Logistic Regression", roc_data)
+save_mean_roc_data(y_test_list, predicted_probs_list["Random Forest"], "Random Forest", roc_data)
+save_mean_roc_data(y_test_list, predicted_probs_list["Naive Bayes"], "Naive Bayes", roc_data)
+
+
+for model_name, data in roc_data.items():
+    df = pd.DataFrame({
+        "FPR": data["mean_fpr"],
+        "TPR": data["mean_tpr"]
+    })
+    csv_path = f"./out/mean_roc_data_{model_name}.csv"
+    df.to_csv(csv_path, index=False)
+
+
+def get_optimal_cutoff(filename):
+    data = pd.read_csv(filename)
+    data["Youdens_Index"] = data["TPR"] - data["FPR"]
+    optimal_cutoff = data.loc[data["Youdens_Index"].idxmax()]
+    
+    return optimal_cutoff
+
+
+model_files = {
+    "SVM": "./out/mean_roc_data_SVM.csv",
+    "Logistic Regression": "./out/mean_roc_data_Logistic Regression.csv",
+    "Random Forest": "./out/mean_roc_data_Random Forest.csv",
+    "GaussianNB": "./out/mean_roc_data_Naive Bayes.csv"
+}
+
+optimal_cutoffs = {}
+
+for model, filename in model_files.items():
+    optimal_cutoff = get_optimal_cutoff(filename)
+    optimal_cutoffs[model] = optimal_cutoff["Youdens_Index"]
+
+with open("./out/optimal_cutoffs.pkl", "wb") as f:
+    pickle.dump(optimal_cutoffs, f)
+
+def save_model(model, filename):
+    with open(filename, "wb") as file:
+        pickle.dump(model, file)
+
+save_model(tuned_models["SVM"], "./out/svm_model.pkl")
+save_model(tuned_models["Logistic Regression"], "./out/logreg_model.pkl")
+save_model(tuned_models["Random Forest"], "./out/rf_model.pkl")
+save_model(tuned_models["Naive Bayes"], "./out/nb_model.pkl")
+
+
+predicted_probs_files = {
+    "svm": predicted_probs_list["SVM"],
+    "logreg": predicted_probs_list["Logistic Regression"],
+    "rf": predicted_probs_list["Random Forest"],
+    "nb": predicted_probs_list["Naive Bayes"]
+}
+
+for model_name, probs_list in predicted_probs_files.items():
+    save_to_pickle(probs_list, f"./out/predicted_probs_{model_name}_list.pkl")
+
